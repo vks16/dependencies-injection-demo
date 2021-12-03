e61205a4e42c9e5ba942258c98fa37e6226100a4
Vishal_Kumar_Sharma <vishalsharma16171718@gmail.com>
2021-12-03 12:49:41 +0530

    DI with setters

--- src/main/java/com/vkswebsolutions/CricketCoach.java
+++ src/main/java/com/vkswebsolutions/CricketCoach.java
@@ -1,13 +1,17 @@
 package com.vkswebsolutions;
 
 public class CricketCoach implements Coach{
+    private FortuneService fortuneService;
+    public void setFortuneService(FortuneService fortuneService){
+        this.fortuneService = fortuneService;
+    }
     @Override
     public String getDailyWorkout() {
-        return null;
+        return "Bat for 30 minutes";
     }
 
     @Override
     public String getFortune() {
-        return null;
+        return fortuneService.getFortune();
     }
 }
